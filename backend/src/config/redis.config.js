2676abeb37bfbda507f815508105cb4f7c051567
Alok Gupta <alokgupta1560@gmail.com>
2024-08-12 17:33:20 +0530

    chore(redis): minor changes in redis config

--- backend/src/config/redis.config.js
+++ backend/src/config/redis.config.js
@@ -1,5 +1,10 @@
 const Redis = require("ioredis");
 
-const redis = new Redis();
+const redis = new Redis({
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+});
 
 module.exports = redis;
